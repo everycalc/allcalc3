33e4d22baea6889f4640310fabbcaba7678c2927
everycalc <everycalculation@gmail.com>
2025-07-26 15:35:45 +0530

    Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { useTheme } from '../contexts/ThemeContext';
 
@@ -17,7 +18,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose, onToggleHistory, onS
 
   useEffect(() => {
     if (isOpen) {
-      fetch('../metadata.json')
+      fetch('/metadata.json')
         .then(response => {
           if (!response.ok) {
             throw new Error('Network response was not ok');
