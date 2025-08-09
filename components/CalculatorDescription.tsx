6e93359733c5522222e936fdea85c3149a2eb0a1
everycalc <everycalculation@gmail.com>
2025-08-09 15:51:35 +0530

    Delete components directory

--- components/CalculatorDescription.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This component is deprecated and split into CalculatorDescriptionContent and CalculatorFaqs.
-// It is kept here to prevent import errors in files not yet updated.
-const CalculatorDescription: React.FC<{ calculatorName: string }> = () => {
-  return null;
-};
-
-export default CalculatorDescription;
--- components/PinningGuide.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const PinningGuide: React.FC = () => {
-  // This is a stub component to prevent import errors for a deprecated/unused file.
-  return null;
-};
-
-export default PinningGuide;
